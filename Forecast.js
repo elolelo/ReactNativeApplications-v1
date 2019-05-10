cc8486c5a27aba43b8fd778cac6dfe847481e3ba
Nompumelelo <lelonompumelelo@gmail.com>
2019-05-10 20:41:16 +0200

    Populated the return function

--- Forecast.js
+++ Forecast.js
@@ -3,7 +3,21 @@ import { StyleSheet, Text, View } from "react-native";
 
 class Forecast extends Component {
     render() {
-       
+        return (
+            <View style={styles.container}>
+                <Text style={styles.bigText}>
+                    {this.props.main}
+                </Text>
+
+                <Text style={styles.mainText}>
+                    Current conditions: {this.props.description}
+                </Text>
+                
+                <Text style={styles.bigText}>
+                    {this.props.temp}°F
+                </Text>
+            </View>
+        ); 
     }
 }
 const styles = StyleSheet.create({
