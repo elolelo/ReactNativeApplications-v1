ac08cb3dcc82c6b25d6c2c3d722ddfb078053011
Nompumelelo <lelonompumelelo@gmail.com>
2019-05-10 21:09:42 +0200

    Added a promise function for the API

--- open_weather_map.js
+++ open_weather_map.js
@@ -1,5 +1,17 @@
 const WEATHER_API_KEY = "";
 const API_STEM = "";
+
 function zipUrl(zip) {
  return `${API_STEM}q=${zip}&units=imperial&APPID=${WEATHER_API_KEY}`;
+}
+
+
+function fetchForecast(zip) {
+    return fetch(zipUrl(zip))
+    .then(response => response.json())
+    .then (responseJSON => {
+        return {
+            
+        }
+    })
 }
